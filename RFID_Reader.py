4e8eeaee7316fdd7d9ab6ebb39f4499b89e77736
pdamianik <39028343+pdamianik@users.noreply.github.com>
2024-03-21 14:44:05 +0100

    fix: rename RFID Reader class

--- RFID_Reader.py
+++ RFID_Reader.py
@@ -3,17 +3,17 @@ import qwiic_rfid
 import time
 import sys
 
+
 def run_example():
-
     print("\nSparkFun Qwiic RFID Reader Example 1")
-    my_RFID = qwiic_rfid.Qwiic_RFID()
+    my_RFID = qwiic_rfid.QwiicRFID()
 
-    if my_RFID.begin() == False:
+    if not my_RFID.begin():
         print("\nThe Qwiic RFID Reader isn't connected to the system. Please check your connection", file=sys.stderr)
         return
-    
+
     print("\nReady to scan some tags!")
-    
+
     while True:
         val = input("\nEnter 1 to get tag ID and scan time: ")
 
@@ -26,8 +26,9 @@ def run_example():
             # If this time is too precise, try:
             # scan_time = my_RFID.get_req_time()
             print("\nScan Time: " + str(scan_time))
-        
+
         time.sleep(0.02)
+
 
 if __name__ == '__main__':
     try:
